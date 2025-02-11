7239d6e7a6b017e3a261983b12af3b05108a794e
L041B <loris.bt02@gmail.com>
2025-02-11 12:14:41 +0100

    Manage_employee

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -35,3 +35,20 @@ def update_user():
     if user_controller.update_user_info(session['id'], field, value):
         return jsonify({'success': True})
     return jsonify({'success': False, 'error': 'Update failed'})
+
+
+@bp.route('/manage_employee', methods=['GET', 'POST'])
+def manage_employee():
+    user_id, user, is_admin, is_company_admin, pending_companies_count = get_user_info()
+    companies= user_controller.get_companies_by_user_id(user_id)
+
+    if not user_id:
+        return redirect(url_for('login'))
+    if is_company_admin:
+        unique_company_admin=user_controller.is_unique_company_admin(user_id)
+        unique_admin=user_controller.get_company_ids_where_user_is_unique_admin(user_id)
+        
+        return render_template('manage_employee.html', user_id=user_id, user=user, is_admin=is_admin, is_company_admin=is_company_admin, unique_company_admin=unique_company_admin, companies=companies, unique_admin=unique_admin, pending_companies_count=pending_companies_count)
+    
+    return render_template('manage_employee.html', user_id=user_id, user=user, is_admin=is_admin, is_company_admin=is_company_admin, companies=companies, pending_companies_count=pending_companies_count)
+
