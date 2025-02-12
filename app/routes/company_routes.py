2bf45e5ffacd6d59e1d510596d5794046b960924
L041B <loris.bt02@gmail.com>
2025-02-12 15:47:06 +0100

    Manage_company

--- app/routes/company_routes.py
+++ app/routes/company_routes.py
@@ -163,3 +163,89 @@ def get_cities():
         return jsonify({'error': 'Data format is incorrect'}), 500
 
     return jsonify(cities_in_country)
+
+@bp.route('/companies_of_administrator', methods=['GET'])
+def companies_administrator():
+    user_id, user, is_admin, is_company_admin, pending_companies_count = get_user_info()
+    
+    try:
+        # Verifica se l'utente è un amministratore di alcune compagnie
+        company_ids = company_controller.get_companyID_by_owner(user_id)  # Otteniamo gli ID delle compagnie
+        # Ora otteniamo le informazioni complete per ogni company_id
+        companies = []
+        for company_id in company_ids:
+            company = company_controller.get_company_by_id(company_id['company_id'])  # Otteniamo i dettagli della compagnia
+            if company:
+                companies.append(company)
+        
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            return jsonify({'companies': companies})
+        
+        return render_template('manage_companies.html', 
+                             companies=companies, 
+                             user_id=user_id, 
+                             user=user, 
+                             is_admin=is_admin, 
+                             is_company_admin=is_company_admin,
+                             pending_companies_count=pending_companies_count)
+    except Exception as e:
+        print(f"Errore durante la ricerca delle compagnie: {e}")
+        return render_template('manage_companies.html', 
+                             companies=[], 
+                             user_id=user_id, 
+                             user=user, 
+                             is_admin=is_admin, 
+                             is_company_admin=is_company_admin)
+
+@bp.route('/modify_company/<int:company_id>', methods=['GET', 'POST'])
+def modify_company(company_id):
+    user_id, user, is_admin, is_company_admin, pending_companies_count = get_user_info()
+    
+    company = company_controller.get_company_by_id(company_id)
+    if company is None:
+        return jsonify({"success": False, "error": "Company not found"}), 404
+
+    if not company['status'] and not is_admin:
+        return jsonify({"success": False, "error": "Unauthorized"}), 403
+
+    if request.method == 'POST':
+        try:
+            # Estrai i dati dalla richiesta
+            company_name = request.form.get('company_name')
+            company_phone_number = request.form.get('company_phone_number')
+            company_email = request.form.get('company_email')
+            company_industry = request.form.get('company_industry')
+            company_website = request.form.get('company_website')
+            company_description = request.form.get('company_description')
+            
+            # Estrai l'immagine (file)
+            company_logo = request.files.get('company_logo') if 'company_logo' in request.files else None # Ottieni il file immagine
+
+            company_image_url=None
+            if company_logo != None:
+                company_image_url = company_controller.upload_company_image(company_id, company_logo)
+            
+
+            # Passa tutti i dati a `update_company`
+            company_controller.update_company(
+                company_id=company_id,
+                company_name=company_name,
+                company_phone_number=company_phone_number,
+                company_email=company_email,
+                company_industry=company_industry,
+                company_website=company_website,
+                company_description=company_description,
+                company_image=company_image_url  # Se c'è un'immagine, passa l'URL, altrimenti None
+            )
+            return jsonify({"success": True})
+        except Exception as e:
+            return jsonify({"success": False, "error": str(e)}), 500
+
+    return render_template('modify_company.html', 
+                         company=company,
+                         user_id=user_id,
+                         user=user,
+                         is_admin=is_admin,
+                         is_company_admin=is_company_admin,
+                         pending_companies_count=pending_companies_count)
+
